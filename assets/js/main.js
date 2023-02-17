b73fc53fdd0874194f84e1b867a3290d4bfb36f6
sskkfmnn <sskkfmnn@gmail.com>
2023-02-17 13:29:46 +0100

    Lev1_1

---
+++ assets/js/main.js
@@ -0,0 +1,19 @@
+console.log ("");
+
+document.write ("<h1>Hello World!</h1>");
+document.write ("<p>Have a nice day!</p>");
+
+let myText = ("Hello again");
+document.write (myText);
+
+document.write ("<br>");
+document.write ("<br>");
+
+let vorname = ("Name: Saskia");
+document.write (vorname);
+
+document.write ("<br>");
+
+let nachname = ("Nachname: Kaufmann");
+document.write (nachname);
+
